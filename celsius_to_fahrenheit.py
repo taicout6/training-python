fe8db75cc7ddd26398775e75ab29cf04ec667a17
Tainá Coutinho <taicout6@gmail.com>
2024-02-01 23:11:02 -0300

    Convertendo Celsius para Fahrenheit

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,12 @@
+def c_to_f(c):
+  f = ((c * 9) / 5) + 32
+  return print(f)
+
+c_to_f(30)
+
+def celsius_to_fahrenheit():
+  c = float(input ('Digite a temperatura em ºC: \n'))
+  f = float((c * 9) / 5) + 32
+  return print('A temperatura em fahrenreit é: ', f, 'ºF')
+
+celsius_to_fahrenheit()
