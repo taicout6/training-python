73fbf337d65620746362b3a76c2dacb8bf5c1ee9
¨Tainá <taicout6@gmail.com>
2024-02-19 10:47:23 -0300

    Aprendendo sobre o comando print()

---
+++ print.py
@@ -0,0 +1,19 @@
+print('Olá galerinha do mal')
+
+##
+x = 4 ** 3
+print('O valor de x é ', x)
+
+##
+print('Imprimindo algo')
+
+##
+print('A funcao print() é muito util para ver se algo está funcionando como deveria')
+
+##
+dia = 10
+mes = 10
+ano = 2024
+print(dia, mes, ano, sep='/')
+print(dia, mes, ano, sep='/', end='...\n')
+
