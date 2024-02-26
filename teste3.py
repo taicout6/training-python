8ee7ea234c55d179e2d40dcc4e52ef7b26be5e8d
¨Tainá <taicout6@gmail.com>
2024-02-26 16:06:04 -0300

    Pequeno ajuste

--- teste3.py
+++ teste3.py
@@ -1,7 +1,5 @@
-perguntaAltura = input('Digite sua altura em metros: ')
+altura = eval(input('Digite sua altura em metros: '))
 genero = input('Digite seu gênero (M/F): ')
-
-altura = eval(perguntaAltura)
 
 if (genero == 'M'):
   pesoIdeal = (72.7 * altura) - 58
@@ -9,3 +7,4 @@ else:
   pesoIdeal = (62.1 * altura) - 44.7
 
 print(f'Seu peso ideal é: {pesoIdeal}Kg')
+
