72b4fc32d5afb67db8becd610da7f8a4bd49ff0a
¨Tainá <taicout6@gmail.com>
2024-02-27 11:33:09 -0300

    Estrutura de condição de três vias

--- teste3.py
+++ teste3.py
@@ -1,10 +1,15 @@
+# Estruturas de condição de uma, duas ou mais vias
+
 altura = eval(input('Digite sua altura em metros: '))
 genero = input('Digite seu gênero (M/F): ')
 
-if (genero == 'M'):
+if genero == 'M' or genero == 'm':
   pesoIdeal = (72.7 * altura) - 58
-else:
+elif genero == 'F' or genero == 'f':
   pesoIdeal = (62.1 * altura) - 44.7
+else:
+  print('Opção inválida')
+
 
 print(f'Seu peso ideal é: {pesoIdeal}Kg')
 
