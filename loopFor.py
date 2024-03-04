bc4e285befde22c8f7137faaff0776716a4f673f
Tainá Coutinho <taicout6@gmail.com>
2024-03-04 13:37:28 -0300

    Função range()

--- loopFor.py
+++ loopFor.py
@@ -12,3 +12,14 @@ for letra in palavra:
   if letra in 'aiueo':
     print(letra)
 
+
+for n in range(10):
+  print(n)
+
+
+for x in range(1, 11):
+  print(x)
+
+for y in range(2, 20, 2):
+  print(y)
+
