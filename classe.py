8846f8280df73d36b445d1d7ec1a2025f150216c
Tainá Coutinho <taicout6@gmail.com>
2024-04-11 13:08:41 -0300

    Classe e Objeto

---
+++ classe.py
@@ -0,0 +1,27 @@
+class Ponto:
+  def __init__(self, x=0, y=0):
+    self.x = x
+    self.y = y
+
+  def setX(self, x):
+    self.x = x
+
+  def setY(self, y):
+    self.y = y
+
+  def get(self):
+    return self.x, self.y
+
+  def mover(self, offsetx, offsety):
+    self.x += offsetx
+    self.y += offsety
+
+  def __repr__(self) -> str:
+    return '(' + str(self.x) + ',' + str(self.y) + ')'
+
+
+p = Ponto()
+p.setX(5)
+p.setY(4)
+
+print(p)
